9fe9b2b0d1a79138ed684416dc28549b29a8424f
Arc <o1fym7@inf.elte.hu>
2022-05-03 12:49:41 +0000

    feat (contact.html): change default form target to mail, add button for sending message via API

---
+++ contact_send.js
@@ -0,0 +1,12 @@
+document
+  .querySelector("#contact-send-api")
+  .addEventListener("click", async function () {
+    const url = new URL(location.origin + "/api/contact");
+    const fdata = new FormData(document.querySelector("form"));
+    for (const key of fdata.keys()) {
+      url.searchParams.append(key, fdata.get(key));        
+    }
+    const r = new URL((await fetch(url)).url);
+    const success = r.searchParams.get("success") == "1" ? true : false;
+    alert(success ? "Message has been successfully sent!" : "Unfortunately, message couldn't be sent. Please try again later.");
+  });
