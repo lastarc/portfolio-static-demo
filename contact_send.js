96012dfce911cc7f5dfb4670336c255ce1da5f50
Arc <o1fym7@inf.elte.hu>
2022-05-04 08:41:07 +0000

    style: format with prettier

--- contact_send.js
+++ contact_send.js
@@ -4,9 +4,13 @@ document
     const url = new URL(location.origin + "/api/contact");
     const fdata = new FormData(document.querySelector("form"));
     for (const key of fdata.keys()) {
-      url.searchParams.append(key, fdata.get(key));        
+      url.searchParams.append(key, fdata.get(key));
     }
     const r = new URL((await fetch(url)).url);
     const success = r.searchParams.get("success") == "1" ? true : false;
-    alert(success ? "Message has been successfully sent!" : "Unfortunately, message couldn't be sent. Please try again later.");
+    alert(
+      success
+        ? "Message has been successfully sent!"
+        : "Unfortunately, message couldn't be sent. Please try again later."
+    );
   });
